cb92c410166dd5120717792c35110b130ae0460f
Anastacio <gl0inn@hotmail.com>
2022-08-15 14:49:20 -0300

    verbose name

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from tabnanny import verbose
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -12,16 +15,17 @@ class Produto(models.Model):
         ('M2', 'Metro Quadrado'),
         ('M3', 'Metro Cubico'),
     )
-    
-    nome = models.CharField(max_length=100, null=False, blank=False)
-    codigo_de_barras = models.BigIntegerField(null=True, blank=True, unique=True)
-    preco_de_venda = models.DecimalField(decimal_places=2, max_digits=10)
-    preco_de_custo = models.DecimalField(decimal_places=2, max_digits=10)
+     
+    nome = models.CharField(verbose_name='Nome',max_length=100, null=False, blank=False)
+    codigo_de_barras = models.BigIntegerField(verbose_name='Código de Barras',null=True, blank=True, unique=True)
+    preco_de_venda = models.DecimalField(verbose_name="Preço de Venda", decimal_places=2, max_digits=10, validators=[MinValueValidator(Decimal('0.01'))])
+    preco_de_custo = models.DecimalField(verbose_name="Preço de Custo", decimal_places=2, max_digits=10, validators=[MinValueValidator(Decimal('0.01'))])
     unidade_de_venda = models.CharField(
+        verbose_name="Unidade de Venda",
         max_length=3,
         choices=UNIDADE_DE_VENDA_CHOICES)
-    data_de_cadastro = models.DateTimeField(auto_now_add=True)
-    ultima_atualizacao = models.DateTimeField(auto_now=True)
+    data_de_cadastro = models.DateTimeField(verbose_name="Data de Cadastro", auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField(verbose_name="Última Atualização", auto_now=True)
     
     def __str__(self):
         return self.nome
