779158eec0218c6d2c4d33613e0d6674ef76a773
Anastacio <gl0inn@hotmail.com>
2022-08-17 17:19:48 -0300

    unique error message

--- api/models.py
+++ api/models.py
@@ -19,7 +19,9 @@ class Produto(models.Model):
     nome = models.CharField(
         verbose_name='Nome',max_length=100, null=False, blank=False)
     codigo_de_barras = models.BigIntegerField(
-        verbose_name='Código de Barras',null=True, blank=True, unique=True)
+        verbose_name='Código de Barras',null=True, blank=True, 
+        unique=True,
+        error_messages={'unique':'Este código de barras já está cadastrado'})
     preco_de_venda = models.DecimalField(
         verbose_name="Preço de Venda", decimal_places=2, max_digits=10, 
         validators=[MinValueValidator(Decimal('0.01'))])
@@ -30,9 +32,11 @@ class Produto(models.Model):
         verbose_name="Unidade de Venda",
         max_length=3,
         choices=UNIDADE_DE_VENDA_CHOICES)
-    data_de_cadastro = models.DateTimeField(verbose_name="Data de Cadastro", auto_now_add=True)
-    ultima_atualizacao = models.DateTimeField(verbose_name="Última Atualização", auto_now=True)
-    
+    data_de_cadastro = models.DateTimeField(
+        verbose_name="Data de Cadastro", auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField(
+        verbose_name="Última Atualização", auto_now=True)
+   
     def __str__(self):
         return self.nome
 
@@ -40,7 +44,10 @@ class Estoque(models.Model):
     
     produto = models.OneToOneField(
         Produto, on_delete=models.CASCADE, related_name='estoque')
-    quantidade = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    quantidade = models.PositiveIntegerField(
+        default=0, 
+        validators=[MinValueValidator(0)], 
+        error_messages={'Erro':'A quantidade deve ser positiva'})
     ultima_entrada = models.DateTimeField(default=None, null=True, blank=True)
     ultima_saida = models.DateTimeField(default=None, null=True, blank=True)
     
