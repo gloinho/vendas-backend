d3873faee041dc7457055eb6a0cde9c0edc30bba
Anastacio <gl0inn@hotmail.com>
2022-08-19 10:52:36 -0300

    list historico

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import CreateProduto, RetrieveUpdateProduto, ListTodosOsProdutos, RetrieveUpdateEstoque, ListTodosOsEstoques
+from api.views import CreateProduto, RetrieveUpdateProduto, ListTodosOsProdutos, RetrieveUpdateEstoque, ListTodosOsEstoques, ListHistorico
 
 app_name = 'api'
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('cadastro',CreateProduto, name="cadastro-de-produto"),
     path('produto/<int:pk>', RetrieveUpdateProduto, name="ver-produto"),
     path('produto/<int:pk>/estoque', RetrieveUpdateEstoque, name="ver-estoque"),
+    path('historicos', ListHistorico.as_view(), name="ver-todos-os-historicos"),
     path('produtos', ListTodosOsProdutos.as_view(), name='ver-todos-os-produtos'),
     path('estoques', ListTodosOsEstoques.as_view(), name='ver-todos-os-estoques')
 ]
